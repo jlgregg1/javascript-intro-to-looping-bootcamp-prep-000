e83356774b33e9db7dbd64adf384cabe0b75f74a
Jennifer Stewart <jlgregg1@gmail.com>
2017-09-16 04:39:51 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -13,3 +13,13 @@ function whileLoop(n) {
   if (countdown === 0) {return "done"}
   }
 }
+
+function doWhileLoop(array) {
+  function maybeTrue() {
+  return Math.random() >= 0.5
+}
+do {
+  array.pop()
+} while (array.length > 0 && maybeTrue());
+return array
+}
