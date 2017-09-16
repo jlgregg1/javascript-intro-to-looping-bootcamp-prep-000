47eebfc749a77f712f36d0be9461bcb5a7681afa
Jennifer Stewart <jlgregg1@gmail.com>
2017-09-16 03:57:38 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,7 +1,7 @@
 for (let i = 0; i < 25; i++) {
   var array = []
   if (i===0) {array = ["I am ${i} strange loops."]}
-  if (i===1) {array = []"I am 1 strange loop."]};
-  else {array = ["I am ${i} strange loops."]};
+  if (i===1) {array = [...array, "I am 1 strange loop."]};
+  else {array = [...array, "I am ${i} strange loops."]};
   return array
 }
